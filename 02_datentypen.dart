a0c0406659fc38c64cb480df18a11c24b10a5898
NickK756 <68076020+NickK756@users.noreply.github.com>
2020-07-14 10:41:28 +0200

    Create 02_datentypen.dart

---
+++ 02_datentypen.dart
@@ -0,0 +1,21 @@
+main()   { 
+  
+  
+String  meinname = "Nick";
+
+print("Hallo ich bin $meinname ");
+  
+  double doub = 3.122122;
+  
+  String cast = doub.toString();
+  
+  print (cast);
+
+int laenge = meinname.laenge;
+  
+  print("Hallo ich bin $meinname");
+
+    print(cast);
+
+print(laenge);
+}
